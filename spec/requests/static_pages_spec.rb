df13e6c21dbd023401f5f534dc3657463ba47aee
Josh Justice <jjustice@bignerdranch.com>
2015-11-25 11:31:11 -0500

    add about page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -20,4 +20,13 @@ describe "StaticPages" do
 
   end
 
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit "/static_pages/about"
+      expect(page).to have_content("About Us")
+    end
+
+  end
+
 end
