8a9e06c14f03651c59340a603bdc77f4fccaf04f
Josh Justice <jjustice@bignerdranch.com>
2015-11-25 14:08:02 -0500

    get signup succeeding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # TODO BLURT
+      redirect_to @user
     else
       render :new
     end
