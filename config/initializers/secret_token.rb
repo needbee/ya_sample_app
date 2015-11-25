f6d350a74d811c38c8d3bf7895fcd49eb0bf066b
Josh Justice <jjustice@bignerdranch.com>
2015-11-25 10:49:31 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YaSampleApp::Application.config.secret_key_base = 'b7eb95c18710862d98c84f240abd59b65ba368001fdb359165a1fd6905a6ddfc1ae728c76eb588d6a94f5b991111b63df3eb20b19d43d025e4233baf7edba235'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YaSampleApp::Application.config.session_store :cookie_store, key: '_ya_sample_app_session'
