412b8a51ef9299e4d3bbbaaa03b30061f1acf61d
Josh Justice <jjustice@bignerdranch.com>
2015-11-25 13:14:00 -0500

    validate email format

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,9 +25,39 @@ describe User do
 
   end
 
-  describe "when email is not present" do
-    before { @user.email = " " }
-    it { should_not be_valid }
+  describe "email" do
+
+    describe "when not present" do
+      before { @user.email = " " }
+      pry
+      it { should_not be_valid }
+    end
+
+    describe "format" do
+
+      describe "invalid" do
+        it "should be invalid" do
+          addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                       foo@bar_baz.com foo@bar+baz.com]
+          addresses.each do |invalid_address|
+            @user.email = invalid_address
+            expect(@user).not_to be_valid
+          end
+        end
+      end
+
+      describe "valid" do
+        it "should be valid" do
+          addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+          addresses.each do |invalid_address|
+            @user.email = invalid_address
+            expect(@user).to be_valid
+          end
+        end
+      end
+
+    end
+
   end
 
 end
