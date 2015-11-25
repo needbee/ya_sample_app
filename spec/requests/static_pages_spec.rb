ce916b63b08c7e87388126083b1cc8ea846c6c97
Josh Justice <jjustice@bignerdranch.com>
2015-11-25 11:31:51 -0500

    add title to home page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,11 @@ describe "StaticPages" do
 
   describe "Home page" do
 
+    it "should have the right title" do
+      visit "/static_pages/home"
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
+
     it "should have the content 'Sample App'" do
       visit "/static_pages/home"
       expect(page).to have_content("Sample App")
