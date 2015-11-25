1467abdf670629225aebc5238258ee6f46ad0dbe
Josh Justice <jjustice@bignerdranch.com>
2015-11-25 12:24:00 -0500

    simplify rspec test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,59 +2,37 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the right title" do
-      visit root_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
-
-    it "should have the content 'Sample App'" do
-      visit root_path
-      expect(page).to have_content("Sample App")
-    end
+    it { should have_title(full_title("Home")) }
+    it { should have_content("Sample App") }
 
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the right title" do
-      visit help_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end
-
-    it "should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_content("Help")
-    end
+    it { should have_title(full_title("Help")) }
+    it { should have_content("Help") }
 
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the right title" do
-      visit about_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
-    end
-
-    it "should have the content 'About Us'" do
-      visit about_path
-      expect(page).to have_content("About Us")
-    end
+    it { should have_title(full_title("About Us")) }
+    it { should have_content("About Us") }
 
   end
 
   describe "Contact page" do
+    before { visit contact_path }
 
-    it "should have the right title" do
-      visit contact_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
-    end
-
-    it "should have the right content" do
-      visit contact_path
-      expect(page).to have_content("Contact")
-    end
+    it { should have_title(full_title("Contact")) }
+    it { should have_content("Contact") }
 
   end
 
